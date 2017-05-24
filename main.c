98ef04d4f26a18ac03b4f1df1c35533eecd14435
Karl Otness <karl@karlotness.com>
2017-05-24 19:43:40 -0400

    Initialize and free time widget.
    Handles interrupt signals and ends event loop.

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
-#include <stdio.h>
 #include <glib.h>
+#include <glib-unix.h>
+
+#include "widgets/time.h"
+
+gboolean interrupt_handler(void *data);
+
+GMainLoop *main_loop;
 
 int main(int argc, char *argv[]) {
-  GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
+  main_loop = g_main_loop_new(NULL, FALSE);
+  gboolean time_status = kbar_time_init(main_loop);
+  // Configure interrupt signal
+  g_unix_signal_add(SIGINT, &interrupt_handler, NULL);
   g_main_loop_run(main_loop);
+  kbar_time_free();
+}
+
+gboolean interrupt_handler(void *data) {
+  g_main_loop_quit(main_loop);
+  return G_SOURCE_REMOVE;
 }
