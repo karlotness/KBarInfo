5a0ee3b4ed96081d8ecb0f8a3fc712fd5915a2ca
Karl Otness <karl@karlotness.com>
2017-06-18 17:30:28 -0400

    Add basic JSON escaping to state output.

--- utils/state.c
+++ utils/state.c
@@ -7,6 +7,7 @@
 #define KBAR_NUM_WIDGETS 1
 const kbar_widget_state *states[1] = {&kbar_time_state};
 
+static void kbar_json_escape(GString *str);
 gboolean kbar_initialized = FALSE;
 
 void kbar_start_print() {
@@ -25,6 +26,7 @@ void kbar_print_bar_state() {
   for(int i = 0; i < KBAR_NUM_WIDGETS; i++) {
     const kbar_widget_state *state = states[i];
     char* urgent = state->urgent ? "true" : "false";
+    kbar_json_escape(state->text);
     printf("{\"urgent\": \"%s\", \"full_text\": \"%s\"}",
            urgent, state->text->str);
     if(i < KBAR_NUM_WIDGETS - 1) {
@@ -33,4 +35,18 @@ void kbar_print_bar_state() {
   }
   puts("],");
   fflush(stdout);
+}
+
+static void kbar_json_escape(GString *str) {
+  if(str->len <= 0) {
+    return;
+  }
+  gchar *c = str->str;
+  while(*c != 0) {
+    if(*c == '"' && (c == str->str || *(c - 1) != '\\')) {
+      g_string_insert(str, c - str->str, "\\");
+      c++;
+    }
+    c++;
+  }
 }
