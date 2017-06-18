48f9a3766f8ac0161a2a4ef5db528b0b8f48a5de
Karl Otness <karl@karlotness.com>
2017-06-18 17:20:02 -0400

    Add DBus utility functions.
    These functions connect to the system and session busses
    and make these objects available to the rest of the program.

---
+++ utils/dbus.c
@@ -0,0 +1,14 @@
+#include "dbus.h"
+
+GDBusConnection *kbar_system_bus;
+GDBusConnection *kbar_session_bus;
+
+void kbar_dbus_init() {
+  kbar_system_bus = g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, NULL);
+  kbar_session_bus = g_bus_get_sync(G_BUS_TYPE_SESSION, NULL, NULL);
+}
+
+void kbar_dbus_free() {
+  g_dbus_connection_close_sync(kbar_system_bus, NULL, NULL);
+  g_dbus_connection_close_sync(kbar_session_bus, NULL, NULL);
+}
---
+++ utils/dbus.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef UTILS_DBUS_H
+#define UTILS_DBUS_H
+
+#include <gio/gio.h>
+
+#define DBUS_PROPERTY_INTERFACE "org.freedesktop.DBus.Properties"
+
+extern GDBusConnection *kbar_system_bus;
+extern GDBusConnection *kbar_session_bus;
+
+void kbar_dbus_init();
+void kbar_dbus_free();
+
+#endif
