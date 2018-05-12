8c21ff27c9c8bad86a475d9de842e7fd5beccda9
Karl Otness <karl@karlotness.com>
2018-05-12 19:18:50 -0400

    Fix incorrect return value from kbar_err_printf.

--- utils/debug.c
+++ utils/debug.c
@@ -23,6 +23,7 @@
 int kbar_err_printf(const char *format, ...) {
   va_list args;
   va_start(args, format);
-  vfprintf(stderr, format, args);
+  int ret = vfprintf(stderr, format, args);
   va_end(args);
+  return ret;
 }
