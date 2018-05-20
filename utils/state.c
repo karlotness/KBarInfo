68e52163afbec228b6b5e0da9114257b7aab6de9
Karl Otness <karl@karlotness.com>
2018-05-20 19:08:53 -0400

    Improve JSON string escaping.
    We now also escape slashes and remove control characters.

--- utils/state.c
+++ utils/state.c
@@ -29,7 +29,7 @@ const kbar_widget_state *states[KBAR_NUM_WIDGETS] =
   {&kbar_network_state, &kbar_volume_state,
    &kbar_power_state, &kbar_time_state};
 
-static void kbar_json_escape(GString *str);
+static gchar *kbar_json_escape(GString *str);
 gboolean kbar_initialized = FALSE;
 
 void kbar_start_print() {
@@ -48,9 +48,12 @@ void kbar_print_bar_state() {
   for(int i = 0; i < KBAR_NUM_WIDGETS; i++) {
     const kbar_widget_state *state = states[i];
     char* urgent = state->urgent ? "true" : "false";
-    kbar_json_escape(state->text);
+    gchar* escaped = kbar_json_escape(state->text);
     printf("{\"urgent\": %s, \"full_text\": \"%s\"}",
-           urgent, state->text->str);
+           urgent, escaped == NULL ? "" : escaped);
+    if(escaped != NULL) {
+      g_free(escaped);
+    }
     if(i < KBAR_NUM_WIDGETS - 1) {
       printf(", ");
     }
@@ -59,16 +62,18 @@ void kbar_print_bar_state() {
   fflush(stdout);
 }
 
-static void kbar_json_escape(GString *str) {
+static gchar *kbar_json_escape(GString *str) {
   if(str->len <= 0) {
-    return;
+    return NULL;
   }
-  gchar *c = str->str;
-  while(*c != 0) {
-    if(*c == '"' && (c == str->str || *(c - 1) != '\\')) {
-      g_string_insert(str, c - str->str, "\\");
-      c++;
+  gchar c;
+  for(gsize loc = 0; loc < str->len && *(str->str + loc) != 0; loc++) {
+    c = *(str->str + loc);
+    if(c < 0x20 || c > 0x7E) {
+      g_string_erase(str, loc, 1);
+      loc--;
     }
-    c++;
   }
+  gchar *escaped = g_strescape(str->str, NULL);
+  return escaped;
 }
