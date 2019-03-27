6f2503d938b3be8fb919dbafddd47ecefee46905
Karl Otness <karl@karlotness.com>
2019-03-27 03:46:39 +0000

    Replace network widget with libnm-powered implementation.

--- widgets/network.c
+++ widgets/network.c
@@ -23,195 +23,130 @@
 #include <NetworkManager.h>
 
 kbar_widget_state kbar_network_state;
-gboolean kbar_network_error, vpn_active;
-guint kbar_network_state_sig, nm_state, *active_conns_sig;
-gsize active_conns_cap, active_conns_count;
-GString *ssid, *vpn;
-GDBusProxy *nm_obj, **active_conns;
+static NMClient *client;
+static gboolean kbar_state_error, kbar_network_error;
+static guint kbar_network_state_sig, kbar_network_conn_add_sig, kbar_network_conn_remove_sig;
+static NMState state;
+static GString *ssid, *vpn;
 
 static void kbar_network_nm_state_sig(GDBusProxy *proxy,
                                       GVariant *changed,
                                       GStrv *invalid,
                                       gpointer data);
-static void kbar_network_nm_conn_sig(GDBusProxy *proxy,
-                                     GVariant *changed,
-                                     GStrv *invalid,
+static void kbar_network_nm_conn_sig(NMClient *cb_client,
+                                     NMActiveConnection *active_connection,
                                      gpointer data);
-static void kbar_network_free_active_conns();
 static void kbar_network_update();
 
 gboolean kbar_network_init() {
-  active_conns = NULL;
-  active_conns_sig = NULL;
-  active_conns_cap = 0;
-  active_conns_count = 0;
+  kbar_state_error = FALSE;
   kbar_network_error = FALSE;
-  GError *err = NULL;
   ssid = g_string_new(NULL);
   vpn = g_string_new(NULL);
-  nm_state = 0;
+  client = nm_client_new(NULL, NULL);
   kbar_widget_state_init(&kbar_network_state);
-  nm_obj = g_dbus_proxy_new_sync(kbar_system_bus,
-                                 G_DBUS_PROXY_FLAGS_NONE,
-                                 NULL,
-                                 NM_DBUS_NAME,
-                                 NM_DBUS_PATH,
-                                 NM_DBUS_INTERFACE,
-                                 NULL,
-                                 &err);
-  if(err != NULL) {
-    kbar_err_printf("DBus network error: %s\n", err->message);
-    kbar_network_error = TRUE;
-    goto exit;
-  }
-  kbar_network_state_sig = g_signal_connect_after(nm_obj,
-                                                  "g-properties-changed",
-                                                  G_CALLBACK(kbar_network_nm_state_sig),
-                                                  NULL);
-  if(kbar_network_state_sig == 0) {
-    kbar_err_printf("Network: Error attaching signal\n");
-    kbar_network_error = TRUE;
-    goto exit;
-  }
-  kbar_network_nm_state_sig(NULL, NULL, NULL, NULL);
-  kbar_network_nm_conn_sig(NULL, NULL, NULL, NULL);
- exit:
-  if(err != NULL) {
-    g_error_free(err);
+  if(!client) {
     return FALSE;
   }
-  return !kbar_network_error;
+  state = NM_STATE_UNKNOWN;
+  // Connect signals
+  kbar_network_state_sig = g_signal_connect(client,
+                                            "notify::state",
+                                            G_CALLBACK(kbar_network_nm_state_sig),
+                                            NULL);
+  kbar_network_conn_add_sig = g_signal_connect(client,
+                                               "active-connection-added",
+                                               G_CALLBACK(kbar_network_nm_conn_sig),
+                                               NULL);
+  kbar_network_conn_remove_sig = g_signal_connect(client,
+                                                  "active-connection-removed",
+                                                  G_CALLBACK(kbar_network_nm_conn_sig),
+                                                  NULL);
+  // Perform initial state update
+  kbar_network_nm_state_sig(NULL, NULL, NULL, NULL);
+  kbar_network_nm_conn_sig(NULL, NULL, NULL);
+  kbar_network_update();
+  return TRUE;
 }
 
 void kbar_network_free() {
-  kbar_network_free_active_conns();
-  g_signal_handler_disconnect(nm_obj, kbar_network_state_sig);
-  g_object_unref(nm_obj);
-  kbar_widget_state_release(&kbar_network_state);
+  g_signal_handler_disconnect(client, kbar_network_state_sig);
+  g_signal_handler_disconnect(client, kbar_network_conn_add_sig);
+  g_signal_handler_disconnect(client, kbar_network_conn_remove_sig);
+  g_object_unref(client);
   g_string_free(ssid, TRUE);
   g_string_free(vpn, TRUE);
+  kbar_widget_state_release(&kbar_network_state);
 }
 
 static void kbar_network_nm_state_sig(__attribute__((unused)) GDBusProxy *proxy,
                                       __attribute__((unused)) GVariant *changed,
                                       __attribute__((unused)) GStrv *invalid,
                                       __attribute__((unused)) gpointer data) {
-  GVariant *state = g_dbus_proxy_get_cached_property(nm_obj,
-                                                     "State");
-  nm_state = g_variant_get_uint32(state);
-  g_variant_unref(state);
-  kbar_network_free_active_conns();
-  // Get new active connections.
-  gsize len;
-  GVariant *active_c_path = g_dbus_proxy_get_cached_property(nm_obj,
-                                                            "ActiveConnections");
-  const gchar **conns_path = g_variant_get_objv(active_c_path, &len);
-  // Ensure we have the space for these objects
-  if(len > active_conns_cap) {
-    // Need to reallocate
-    active_conns = g_renew(GDBusProxy*, active_conns, len);
-    active_conns_sig = g_renew(guint, active_conns_sig, len);
-  }
-  // Connect to the relevant objects
-  for(gsize i = 0; i < len; i++) {
-    GError *err = NULL;
-    active_conns[i] = g_dbus_proxy_new_sync(kbar_system_bus,
-                                            G_DBUS_PROXY_FLAGS_NONE,
-                                            NULL,
-                                            NM_DBUS_NAME,
-                                            conns_path[i],
-                                            NM_DBUS_CONN_INTERFACE,
-                                            NULL,
-                                            &err);
-    if(err != NULL) {
-      // Error occurred
-      g_error_free(err);
-      continue;
-    }
-    active_conns_sig[i] = g_signal_connect_after(active_conns[i],
-                                                 "g-properties-changed",
-                                                 G_CALLBACK(&kbar_network_nm_conn_sig),
-                                                 NULL);
-    active_conns_count++;
-  }
-  kbar_network_nm_conn_sig(NULL, NULL, NULL, NULL);
-  g_free(conns_path);
-  g_variant_unref(active_c_path);
+  state = nm_client_get_state(client);
+  kbar_state_error = FALSE;
   kbar_network_update();
 }
 
-static void kbar_network_free_active_conns() {
-  for(gsize i = 0; i < active_conns_count; i++) {
-    if(active_conns_sig[i] > 0) {
-      g_signal_handler_disconnect(active_conns[i],
-                                  active_conns_sig[i]);
-      active_conns_sig[i] = 0;
-    }
-    g_object_unref(active_conns[i]);
-    active_conns[i] = NULL;
-  }
-  active_conns_count = 0;
-  g_string_assign(ssid, "");
-  g_string_assign(vpn, "");
-  vpn_active = FALSE;
-}
-
-static void kbar_network_nm_conn_sig(__attribute__((unused)) GDBusProxy *proxy,
-                                     __attribute__((unused)) GVariant *changed,
-                                     __attribute__((unused)) GStrv *invalid,
+static void kbar_network_nm_conn_sig(__attribute__((unused)) NMClient *cb_client,
+                                     __attribute__((unused)) NMActiveConnection *active_connection,
                                      __attribute__((unused)) gpointer data) {
-  // One of the current connections changed. Re-query all.
-  g_string_assign(ssid, "");
-  g_string_assign(vpn, "");
-  vpn_active = FALSE;
-  for(gsize i = 0; i < active_conns_count; i++) {
-    GDBusProxy *obj = active_conns[i];
-    g_object_ref(obj);
-    if(obj == NULL) {
-      continue;
+  const GPtrArray *connections = nm_client_get_active_connections(client);
+  if(connections == NULL) {
+    goto error;
+  }
+  vpn = g_string_set_size(vpn, 0);
+  for(guint i = 0; i < connections->len; i++) {
+    NMActiveConnection *conn = g_ptr_array_index(connections, i);
+    if(!conn) {
+      goto error;
     }
-    GVariant *type_v = g_dbus_proxy_get_cached_property(obj, "Type");
-    GVariant *id_v = g_dbus_proxy_get_cached_property(obj, "Id");
-    gchar const *type = g_variant_get_string(type_v, NULL);
-    gchar const *id = g_variant_get_string(id_v, NULL);
-    if(g_strcmp0(type, "vpn") == 0) {
-      // VPN connection. Report it.
-      g_string_assign(vpn, id);
+    const char *type, *name;
+    type = nm_active_connection_get_connection_type(conn);
+    name = nm_active_connection_get_id(conn);
+    if(!type || !name) {
+      goto error;
     }
-    else if(g_strcmp0(type, "tun") == 0) {
-      // Tunnel connection (ignore)
-      vpn_active = TRUE;
+    if(strncmp(type, "802-11-wireless", 15) == 0) {
+      g_string_assign(ssid, name);
+    }
+    else if(strncmp(type, "wireguard", 9) == 0 || strncmp(type, "vpn", 3) == 0) {
+      if(vpn->len > 0){
+        g_string_append(vpn, ", ");
+      }
+      g_string_append(vpn, name);
     }
     else {
-      // Probably the "primary" connection we want.
-      g_string_assign(ssid, id);
+      continue;
     }
-    g_variant_unref(type_v);
-    g_variant_unref(id_v);
-    g_object_unref(obj);
   }
   kbar_network_error = FALSE;
   kbar_network_update();
+  return;
+ error:
+  kbar_network_error = TRUE;
+  kbar_network_update();
+  return;
 }
 
 static void kbar_network_update() {
-  kbar_network_state.urgent = kbar_network_error;
+  kbar_network_state.urgent = kbar_network_error || kbar_state_error;
   if(kbar_network_error) {
     g_string_assign(kbar_network_state.text, "N: err");
     kbar_print_bar_state();
     return;
   }
   gchar *c_str;
-  if(g_strcmp0(ssid->str, "") == 0) {
+  if(ssid->len == 0) {
     g_string_assign(ssid, "--");
   }
-  if(nm_state <= 50 && nm_state >= 40) {
+  if(state != NM_STATE_CONNECTED_GLOBAL && state != NM_STATE_CONNECTED_SITE && state != NM_STATE_CONNECTED_LOCAL) {
     c_str = "~ ";
   }
   else {
     c_str = " ";
   }
-  if(g_strcmp0(vpn->str, "") != 0 && vpn_active) {
+  if(g_strcmp0(vpn->str, "") != 0 && vpn->len > 0) {
     // VPN is connected
     g_string_printf(kbar_network_state.text, "N:%s%s (%s)", c_str, ssid->str, vpn->str);
   }
