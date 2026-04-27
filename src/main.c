cd554a7d279bf4851122a9cf4754fc02bb856a41
Karl Otness <karl@karlotness.com>
2026-04-27 16:09:59 -0400

    Remove global variable storing the main loop.
    Also remove additional idle function call while shutting down the
    event loop. Instead, just stop the loop directly.

--- src/main.c
+++ src/main.c
@@ -28,7 +28,10 @@
 #include "volume.h"
 #include "network.h"
 
-static GMainLoop *main_loop = NULL;
+struct KBarMainState {
+  GMainLoop *main_loop;
+  KBarStatusBar *status_bar;
+};
 
 static gboolean kbar_idle_loop_start(void *data) {
   KBarStatusBar *status_bar = data;
@@ -37,17 +40,12 @@ static gboolean kbar_idle_loop_start(void *data) {
   return G_SOURCE_REMOVE;
 }
 
-static gboolean kbar_idle_loop_stop(void *data) {
-  KBarStatusBar *status_bar = data;
-  kbar_statusbar_end_print(status_bar);
-  if(main_loop) {
-    g_main_loop_quit(main_loop);
-  }
-  return G_SOURCE_REMOVE;
-}
-
 static gboolean kbar_interrupt_handler(void *data) {
-  g_idle_add(kbar_idle_loop_stop, data);
+  struct KBarMainState *main_state = data;
+  kbar_statusbar_end_print(main_state->status_bar);
+  if(main_state->main_loop) {
+    g_main_loop_quit(main_state->main_loop);
+  }
   return G_SOURCE_CONTINUE;
 }
 
@@ -64,27 +62,28 @@ static gboolean kbar_signal_resume(void *data) {
 }
 
 int main(void) {
-  main_loop = g_main_loop_new(NULL, FALSE);
+  struct KBarMainState main_state = {0};
+  main_state.main_loop = g_main_loop_new(NULL, FALSE);
   // Construct widgets
-  KBarStatusBar *status_bar = kbar_statusbar_new();
+  main_state.status_bar = kbar_statusbar_new();
   // Configure signal handlers
-  guint sigint_id = g_unix_signal_add(SIGINT, kbar_interrupt_handler, status_bar);
-  guint sigusr1_id = g_unix_signal_add(SIGUSR1, kbar_signal_pause, status_bar);
-  guint sigusr2_id = g_unix_signal_add(SIGUSR2, kbar_signal_resume, status_bar);
+  guint sigint_id = g_unix_signal_add(SIGINT, kbar_interrupt_handler, &main_state);
+  guint sigusr1_id = g_unix_signal_add(SIGUSR1, kbar_signal_pause, main_state.status_bar);
+  guint sigusr2_id = g_unix_signal_add(SIGUSR2, kbar_signal_resume, main_state.status_bar);
   // Add widgets
-  kbar_statusbar_take_widget(status_bar, KBAR_WIDGET(kbar_widget_network_new()));
-  kbar_statusbar_take_widget(status_bar, KBAR_WIDGET(kbar_widget_volume_new()));
-  kbar_statusbar_take_widget(status_bar, KBAR_WIDGET(kbar_widget_power_new()));
-  kbar_statusbar_take_widget(status_bar, KBAR_WIDGET(kbar_widget_time_new()));
+  kbar_statusbar_take_widget(main_state.status_bar, KBAR_WIDGET(kbar_widget_network_new()));
+  kbar_statusbar_take_widget(main_state.status_bar, KBAR_WIDGET(kbar_widget_volume_new()));
+  kbar_statusbar_take_widget(main_state.status_bar, KBAR_WIDGET(kbar_widget_power_new()));
+  kbar_statusbar_take_widget(main_state.status_bar, KBAR_WIDGET(kbar_widget_time_new()));
   // Run loop
-  g_idle_add(kbar_idle_loop_start, status_bar);
-  g_main_loop_run(main_loop);
+  g_idle_add(kbar_idle_loop_start, main_state.status_bar);
+  g_main_loop_run(main_state.main_loop);
   // Destroy widgets and return
   g_source_remove(sigusr2_id);
   g_source_remove(sigusr1_id);
   g_source_remove(sigint_id);
-  g_clear_object(&status_bar);
-  g_main_loop_unref(main_loop);
-  main_loop = NULL;
+  g_clear_object(&main_state.status_bar);
+  g_main_loop_unref(main_state.main_loop);
+  main_state.main_loop = NULL;
   return 0;
 }
